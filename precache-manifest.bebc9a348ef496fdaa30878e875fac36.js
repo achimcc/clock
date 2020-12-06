3399ecfb0f044697f367429f69d46909454b0521
Achim <achim.schneider@equeo.de>
2020-12-06 22:33:54 +0100

    Updates

---
+++ precache-manifest.bebc9a348ef496fdaa30878e875fac36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb8ff81fab65f3abc8144ee34e2a478",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2f69920b84fa4c335e9",
+    "url": "./static/css/main.e1465e0a.chunk.css"
+  },
+  {
+    "revision": "8d07a27620f99bf5f159",
+    "url": "./static/js/2.05ed1291.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.05ed1291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f69920b84fa4c335e9",
+    "url": "./static/js/main.6cf69be5.chunk.js"
+  },
+  {
+    "revision": "64249bf4b8f42d1db2b3",
+    "url": "./static/js/runtime-main.6da5ff66.js"
+  }
+]);
